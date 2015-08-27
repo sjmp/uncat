11100e70c98559d71c52fe752650237c05266080
sjmp <samueljamespartridge@gmail.com>
2015-08-27 19:36:30 +0200

    Details more filled out. Need to get display right to work and graph in correctly. Select upcoming

--- app/components/details/detailsController.js
+++ app/components/details/detailsController.js
@@ -6,10 +6,25 @@
             var self = this;
             self.tab = 'Pie';
         
-            self.piedata = [
-                {label: "Sample Value", value: 80, suffix: "%", color: "blue"}
+            self.bigpiedata = [
+                {label: "one", value: 12.2, color: "red"}, 
+                {label: "two", value: 45, color: "#00ff00"},
+                {label: "three", value: 10, color: "rgb(0, 0, 255)"}
             ];
-            self.pieoptions = {thickness: 40, mode: "gauge", total: 100};
-                
+            
+            self.leftpiedata = [
+                {label: "one", value: 10, color: "red"}, 
+                {label: "two", value: 15, color: "blue"},
+                {label: "three", value: 50, color: "green"},
+                {label: "four", value: 20, color: "yellow"},
+                {label: "five", value: 5, color: "purple"}
+            ];
+            
+            self.guagedata = [
+                {label: "Guage", value: 155, color: "#d62728", suffix: "%"}
+                ];
+            
+            self.pieoptionsA = {thickness: 80};
+            self.guageoptions = {thickness: 40, mode: "gauge", total: 360};
     });
 }());
